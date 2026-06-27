51d9d2c7b1fae233d37111177ad8904ebfa746a0
thomas-reed <thomas.h.reed@gmail.com>
2026-06-26 20:16:47 -0400

    Tabbing issue

--- cmd/ufos/main.go
+++ cmd/ufos/main.go
@@ -17,23 +17,23 @@ func main() {
 
 	// build command registry
 	cmds := client.Commands{
-	Registry: make(map[string]client.CommandInfo),
-}
+		Registry: make(map[string]client.CommandInfo),
+	}
 
-cmds.Register("help", cmds.PrintHelp, "List available commands")
-cmds.Register("health", c.HandleHealth, "Check server health at the given --domain (-d)")
-cmds.Register("init", c.HandleInit, "Create a new vault and register your first persona")
-cmds.Register("new", c.HandleNewPersona, "Get a new registration token from the server to create a new persona")
-cmds.Register("register", c.HandleCreatePersona, "Register a persona with the server")
-cmds.Register("list", c.HandleList, "List UFOs with the given --prefix (-p)")
-cmds.Register("search", c.HandleSearch, "Search for UFOs by --tag (-t), and limit to a given --prefix (-p)")
-cmds.Register("upload", c.HandleUploadUFO, "Upload a new UFO")
-cmds.Register("download", c.HandleDownloadUFO, "Download one of your own UFOs")
-cmds.Register("fetch", c.HandleFetchUFO, "Fetch a shared UFO from another host")
-cmds.Register("remove", c.HandleRemoveUFO, "Remove a UFO")
-cmds.Register("update", c.HandleUpdateUFO, "Update a UFO")
-cmds.Register("details", c.HandleUFODetails, "Show UFO metadata")
-cmds.Register("orbit", c.HandleOrbitCmds, "Manage your orbit. Subcommands: add, update, list, details, remove")
+	cmds.Register("help", cmds.PrintHelp, "List available commands")
+	cmds.Register("health", c.HandleHealth, "Check server health at the given --domain (-d)")
+	cmds.Register("init", c.HandleInit, "Create a new vault and register your first persona")
+	cmds.Register("new", c.HandleNewPersona, "Get a new registration token from the server to create a new persona")
+	cmds.Register("register", c.HandleCreatePersona, "Register a persona with the server")
+	cmds.Register("list", c.HandleList, "List UFOs with the given --prefix (-p)")
+	cmds.Register("search", c.HandleSearch, "Search for UFOs by --tag (-t), and limit to a given --prefix (-p)")
+	cmds.Register("upload", c.HandleUploadUFO, "Upload a new UFO")
+	cmds.Register("download", c.HandleDownloadUFO, "Download one of your own UFOs")
+	cmds.Register("fetch", c.HandleFetchUFO, "Fetch a shared UFO from another host")
+	cmds.Register("remove", c.HandleRemoveUFO, "Remove a UFO")
+	cmds.Register("update", c.HandleUpdateUFO, "Update a UFO")
+	cmds.Register("details", c.HandleUFODetails, "Show UFO metadata")
+	cmds.Register("orbit", c.HandleOrbitCmds, "Manage your orbit. Subcommands: add, update, list, details, remove")
 
 	// parse cmd line arguments
 	if len(os.Args) < 2 {
